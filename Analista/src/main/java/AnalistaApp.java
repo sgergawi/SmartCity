943091d24c80f56646ac162f287b7af971678269
Sandra Gergawi <sandragergawi@MBP-di-Sandra.homenet.telecomitalia.it>
2018-06-26 20:28:00 +0200

    Analista initialized with a sample of REST communication

---
+++ Analista/src/main/java/AnalistaApp.java
@@ -0,0 +1,55 @@
+import analista.functionalities.FunctController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class AnalistaApp {
+    public static void main(String[] args){
+        boolean anotherChoice= true;
+        while(anotherChoice){
+
+            System.out.println("Scegliere una delle seguenti opzioni: ");
+            System.out.println("1. Conoscere lo stato attuale della città");
+            System.out.println("2. Conoscere le ultime N statistiche (con timestamp) da uno specifico nodo");
+            System.out.println("3. Conoscere le ultime N statistiche (con timestamp) globali e locali della città");
+            System.out.println("4. Deviazione std e media delle ultime N statistiche prodotte da uno specifico nodo");
+            System.out.println("5. Deviazione std e media delle ultime N statistiche globali della città");
+            System.out.print("Opzione scelta: ");
+            try {
+                BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+                String option = bufReader.readLine();
+                if(option!=null && option.trim().equalsIgnoreCase("Q")){
+                    System.out.println("L'applicativo sta per terminare ...");
+                    anotherChoice = false;
+                } else{
+                    Integer choice = Integer.valueOf(option);
+                    FunctController functs = new FunctController();
+
+                    switch (choice){
+                        case 1:
+                            functs.getCityState();
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        default:
+                            System.out.println("Input non riconosciuto");
+                            break;
+                    }
+                }
+                System.out.println("--------------------------------");
+                System.out.println("\n\n");
+
+
+            } catch (IOException io) {
+                System.out.println("Errore nella ricezione dell'input.");
+            } catch (Exception e) {
+                System.out.println("Si è verificato un errore.");
+            }
+        }
+
+    }
+}
---
+++ Analista/src/main/java/analista/functionalities/FunctController.java
@@ -0,0 +1,68 @@
+package analista.functionalities;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import java.util.Scanner;
+public class FunctController {
+
+    public int getStatsNumber(Scanner scanner) throws Exception {
+        System.out.print("Inserire il numero di statistiche desiderate: ");
+        try{
+            int statsNumber = scanner.nextInt();
+            return statsNumber;
+        } catch(Exception e) {
+           throw e;
+        }
+    }
+    public void getCityState(){
+        Client client = Client.create();
+        WebResource webResource = client.resource("http://localhost:8080/cloud-server/city-states");
+        ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+        String output=response.getEntity(String.class);
+        System.out.println("Ho ricevuto: "+output);
+    }
+
+    public void getEdgeNodeStats(){
+
+        try{
+            Scanner scanner = new Scanner(System.in);
+            int statsNumber = getStatsNumber(scanner);
+            System.out.print("Inserire l'id del nodo edge: ");
+            String edgeId = scanner.next();
+        } catch(Exception io){
+            System.out.println("Errore nella ricezione dei dati in input");
+        }
+
+    }
+
+    public void getGlobalAndLocalStats(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            int statsNumber = getStatsNumber(scanner);
+
+        } catch(Exception e){
+            System.out.println("Errore nella ricezione dei dati in input");
+        }
+    }
+
+    public void getStdDevMeanSingleNode(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            int statsNumber = getStatsNumber(scanner);
+            System.out.print("Inserire l'ID del nodo edge interessato: ");
+            String nodeId = scanner.next();
+        } catch( Exception io){
+            System.out.println("Errore nella ricezione dei dati in input");
+        }
+    }
+    public void getStdDevMeanNodes(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            int statsNumber = getStatsNumber(scanner);
+        } catch( Exception io){
+            System.out.println("Errore nella ricezione dei dati in input");
+        }
+    }
+}
